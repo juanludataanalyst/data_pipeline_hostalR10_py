5d13bcff229c07ea0206bc611c2e3758a9f9c72c
Juan Luis Perez Carretero <juanludataanalyst@gmail.com>
2024-08-20 16:51:42 -0500

    First remote repository commit: Zak API calls working creating dataframes and API KEY as environment variable in a configuration file

--- main.py
+++ main.py
@@ -7,47 +7,21 @@ import pandas as pd
 created = {'from': '01/08/2023', 'to': '15/08/2024'}
 arrival = {'from': '14/08/2024', 'to': '15/08/2024'}
 
-
-
-print("Ejecuta Reservations Data")
+# Calling apis for reservations and room names
 reservations_data = get_reservations_data(created,arrival)
-
-
-print("Ejecuta Get Room Name Type")
 room_info = get_room_name_type()
 
+# Converting raw data to dataframes
 reservations_data_df = pd.DataFrame(reservations_data['reservations'])
 room_reservas_df = pd.DataFrame(reservations_data['room_reservas'])
 customers_df = pd.DataFrame(reservations_data['customers'])
 room_info_df = pd.DataFrame(room_info)
 
-
+# Getting customer_ids and calling api for getting information
 customer_ids = customers_df['id_customer'].tolist()  # Get customer id's list from customer dataframe
-print("Ejecuta get customer info")
-#customer_data = [get_customer_info(customer_id) for customer_id in customer_ids] # List comprension, for iteration and get a dictionary list
-
-customer_data = []
-count = 0
-for customer_id in customer_ids:
-    customer_data.append(get_customer_info(customer_id))
-    count += 1
-    print(f"Customer {count}")
-
+customer_data = [get_customer_info(customer_id) for customer_id in customer_ids] # List comprension, for iteration and get a dictionary list
 
 customer_data_info_df = pd.DataFrame(customer_data)
-print("Sale de  get customer info")
-
-print("reservations_data_df")
-print(reservations_data_df)
-print("room_reservas_df")
-print(room_reservas_df)
-print("customers_df")
-print(customers_df)
-print("(room_info_df")
-print(room_info_df)
-print("customer_data_df")
-print(customer_data_info_df)
-
 
 
 dataframes = {
--- save_data_funcionts.py
+++ save_data_funcionts.py
@@ -2,15 +2,13 @@ import os
 
 def save_dataframes_to_csv(dataframes, output_dir):
     """
-    Guarda múltiples DataFrames en archivos CSV separados.
+    Saves multiple DataFrames to separate CSV files.
 
     Args:
-        dataframes: Un diccionario donde las claves son los nombres de los DataFrames y los valores son los DataFrames.
-        output_dir: La ruta al directorio donde se guardarán los archivos.
+        dataframes (dict): A dictionary where keys are DataFrame names and values are DataFrames.
+        output_dir (str): The path to the directory where the files will be saved.
     """
 
     for df_name, df in dataframes.items():
         file_path = os.path.join(output_dir, f"{df_name}.csv")
         df.to_csv(file_path, index=False)
-
-#
