4b8827331bfc642b426ad7fef7d803246ac699e5
Juan Luis Perez Carretero <juanludataanalyst@gmail.com>
2024-08-20 21:10:01 -0500

    Bucle que devuelve los datos cada 15 dias en una lista de dataframes

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import datetime
 
 def get_data_for_period(created_start, created_end, arrival_start, arrival_end):
   """
-  Realiza una llamada a la API en un rango de fechas especificado para los campos created y arrival,
-  y devuelve los dataframes.
+  Makes an API call within a specified date range for the created and arrival fields,
+  and returns the dataframes.
 
   Args:
-      created_start (datetime.datetime): Fecha de inicio del rango para el campo created.
-      created_end (datetime.datetime): Fecha de fin del rango para el campo created.
-      arrival_start (datetime.datetime): Fecha de inicio del rango para el campo arrival.
-      arrival_end (datetime.datetime): Fecha de fin del rango para el campo arrival.
+      created_start (datetime.datetime): Start date of the range for the created field.
+      created_end (datetime.datetime): End date of the range for the created field.
+      arrival_start (datetime.datetime): Start date of the range for the arrival field.
+      arrival_end (datetime.datetime): End date of the range for the arrival field.
 
   Returns:
-      dict: Diccionario con los cuatro dataframes: reservations, room_reservas, customers, customer_data_info.
+      dict: Dictionary with the four dataframes: reservations, room_reservas, customers, customer_data_info.
   """
 
   # Set filter parameters for the given period
@@ -27,8 +27,11 @@ def get_data_for_period(created_start, created_end, arrival_start, arrival_end):
 
   # Converting raw data to dataframes
   reservations_data_df = pd.DataFrame(reservations_data['reservations'])
+  print("reservations_data_df ")
+  print(reservations_data_df )
   room_reservas_df = pd.DataFrame(reservations_data['room_reservas'])
   customers_df = pd.DataFrame(reservations_data['customers'])
+
   room_info_df = pd.DataFrame(room_info)
 
   # Getting customer_ids and calling api for getting information
@@ -47,11 +50,34 @@ def get_data_for_period(created_start, created_end, arrival_start, arrival_end):
   }
 
 # Example usage with more specific filtering
-created_start = datetime.datetime(2024, 8, 19)
+created_start = datetime.datetime(2024, 1, 1)
 created_end = datetime.datetime(2024, 8, 20)
-arrival_start = datetime.datetime(2024, 8, 19)
+arrival_start = datetime.datetime(2024, 7, 19)
 arrival_end = datetime.datetime(2024, 8, 20)
 
-dataframes = get_data_for_period(created_start, created_end, arrival_start, arrival_end)
-print(dataframes)
 
+dataframes_list = []
+start_date = arrival_start
+
+while start_date <= arrival_end:
+    # Calcular la fecha de fin del período quincenal
+    loop_date = start_date + datetime.timedelta(days=14)
+   
+    # Asegurarse de que biweekly_end_date no exceda la fecha de fin general
+    loop_date = min(loop_date, arrival_end)
+
+      
+    dataframes = get_data_for_period( created_start,  created_end,start_date,loop_date)
+    print(dataframes)
+    dataframes_list.append(dataframes)
+    
+    # Actualizar start_date para el siguiente período quincenal
+    start_date = loop_date + datetime.timedelta(days=1)
+    print("start_date")
+    print(start_date)
+
+
+
+print(dataframes_list)
+
+#dataframes = get_data_for_period(created_start, created_end, arrival_start, arrival_end)
